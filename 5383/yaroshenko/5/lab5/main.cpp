7483a8d4ffdc7c60540ba91b792a66c23f4cc617
Iterus <supermax705@gmail.com>
2017-05-07 01:04:20 +0300

    Подключение файлов 2-й лабы

--- 5383/yaroshenko/5/lab5/main.cpp
+++ 5383/yaroshenko/5/lab5/main.cpp
@@ -1,3 +1,6 @@
+#include "../../2/Lab2Tests/src/square.h"
+#include "../../2/Lab2Tests/src/righttriangle.h"
+#include "../../2/Lab2Tests/src/isoscelestriangle.h"
 #include "../../3/vector.h"
 #include "../../4/shared_ptr.h"
 
