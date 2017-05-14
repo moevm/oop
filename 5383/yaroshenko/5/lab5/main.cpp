8c9573eb0c52dacc40fcef27b0a371c9bb0952b4
Iterus <supermax705@gmail.com>
2017-05-14 20:43:18 +0300

    Пример использования методов

--- 5383/yaroshenko/5/lab5/main.cpp
+++ 5383/yaroshenko/5/lab5/main.cpp
@@ -1,13 +1,33 @@
-#include "../../2/Lab2Tests/src/square.h"
-#include "../../2/Lab2Tests/src/righttriangle.h"
-#include "../../2/Lab2Tests/src/isoscelestriangle.h"
-#include "../../3/vector.h"
-#include "../../4/shared_ptr.h"
+#include "lab2.h"
 
-using namespace stepik;
+bool search_predicate(Shape* s, Shape* cmp)
+{
+    return (s->area() < cmp->area());
+}
+
+bool divide_predicate(Shape* s)
+{
+    return (s->area() > 100);
+}
 
 int main()
 {
+    stepik::vector< stepik::shared_ptr<Shape> > shapes = generateShapes(1000);
+
+    // несколько заданных фигур для поиска
+    stepik::vector< stepik::shared_ptr<Shape> > searchShapes =
+    {
+        stepik::shared_ptr<Shape>(new Square(Point2d(), 25)),
+        stepik::shared_ptr<Shape>(new Rectangle(Point2d(), 30, 40)),
+        stepik::shared_ptr<Shape>(new RightTriangle(Point2d(), 10, 30))
+    };
+
+    // ищем первую фигуру из нескольких по критерию
+    stepik::shared_ptr<Shape> searchResult = multipleSearch(shapes, searchShapes, search_predicate);
+
+    // разбиваем массив на 2 части по критерию
+    stepik::vector< stepik::shared_ptr<Shape> > part1, part2;
+    divideVector(shapes, part1, part2, divide_predicate);
 
     return 0;
 }
