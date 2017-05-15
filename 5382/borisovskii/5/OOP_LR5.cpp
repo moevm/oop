dc9872e32e111593789f88e2ea133fde6c0b9f0b
borisovskii <vender98@mail.ru>
2017-05-15 19:01:03 +0300

    Исправление замечания

--- 5382/borisovskii/5/OOP_LR5.cpp
+++ 5382/borisovskii/5/OOP_LR5.cpp
@@ -12,11 +12,11 @@
 const size_t definite_square = 200;
 
 bool binary_pred(const shared_ptr<Shape> &a, const shared_ptr<Shape> &b) {
-	return b.get()->isInsideOfAnother(*(a.get()));
+	return b->isInsideOfAnother(*a);
 }
 
 bool unary_pred(const shared_ptr<Shape> &a) {
-	return a.get()->square() > definite_square;
+	return a->square() > definite_square;
 }
 
 my_vector<shared_ptr<Shape>> generate_objects() {
@@ -54,7 +54,7 @@ TEST(Algorithm_tests, Non_modifying)
 	auto it = search_n(array.begin(), array.end(), count, check, binary_pred);
 	if (it != array.end())
 		for (size_t i = 0; i != count; ++i, ++it) {
-			EXPECT_TRUE(check.get()->isInsideOfAnother(*(it->get() )));
+			EXPECT_TRUE(check->isInsideOfAnother(*(it->get() )));
 		}
 }
 
@@ -66,11 +66,11 @@ TEST(Algorithm_tests, Modifying)
 	copy_if(array.begin(), array.end(), stdext::make_unchecked_array_iterator(result.begin()), unary_pred);
 
 	auto it = result.begin();
-	for (; ((it->get() != nullptr) && (it != result.end())); ++it);
+	for (; (( it->get() != nullptr) && (it != result.end())); ++it);
 	result.resize(it - result.begin());
 
 	for (it = result.begin(); it != result.end(); ++it) {
-		EXPECT_TRUE(it->get()->square() > definite_square);
+		EXPECT_TRUE((*it)->square() > definite_square);
 	}
 }
 
@@ -82,7 +82,6 @@ GTEST_API_ int main(int argc, char **argv) {
 	srand(time(0));
 	testing::InitGoogleTest(&argc, argv);
 	RUN_ALL_TESTS();
-
 	system("pause");
 	return 0;
 }
