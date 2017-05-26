36e958a8645efb0044efdaec3ceb1bf42bc0a86e
Vivitoria <nika_05_10@mail.ru>
2017-05-26 15:13:12 +0300

    lab5 initial

---
+++ 5383/Filippova/5/5.h
@@ -0,0 +1,68 @@
+#include "../../2/src/ellipse.h"
+#include "../../2/src/square.h" // + rectangle.h
+#include "../../3/list.h"
+#include "../../4/shared_ptr.h"
+#include <functional>
+
+namespace stepik
+{
+
+
+bool predicate(Shape* shape)
+{
+    // расстояние от начала координат до ближайшей вершины больше 20
+    return (shape->distance(Point()) > 20);
+}
+
+
+list_iterator< shared_ptr<Shape> > search_n(
+        const list< shared_ptr<Shape> >& container,
+        size_t count,
+        std::function<bool (Shape*)> pred)
+{
+    size_t curr_count = 0;
+
+    list_iterator< shared_ptr<Shape> > result,
+            first = container.begin(),
+            last = container.end();
+
+    for (; first != last; first++)
+    {
+        curr_count = 0;
+        result = first;
+
+        while (pred(first->get()))
+        {
+            curr_count++;
+            if (curr_count == count)
+                return result;
+
+            first++;
+        }
+    }
+
+    return last;
+}
+
+
+list< shared_ptr<Shape> > copy_if(
+        const list< shared_ptr<Shape> >& container,
+        std::function<bool (Shape*)> pred)
+{
+    list< shared_ptr<Shape> > result;
+    list_iterator< shared_ptr<Shape> > first = container.begin(), last = container.end();
+
+    while (first != last)
+    {
+        if (pred(first->get()))
+        {
+            result.push_back(*first);
+        }
+        first++;
+    }
+
+    return result;
+}
+
+
+}
