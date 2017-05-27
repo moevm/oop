d15e756b5182997dd77445603707e893e380cbd6
OlyaVladimirova <milkis97@gmail.com>
2017-05-27 04:55:10 +0300

    Лабораторная работа №5

---
+++ 5382/vladimirova/5/Shared_ptr.h
@@ -0,0 +1,238 @@
+#pragma once
+
+template <typename T>
+class shared_ptr
+{
+public:
+
+	explicit shared_ptr(T *ptr = 0)
+		:smart(ptr)
+	{
+		if (smart == 0) count = nullptr;
+		else count = new long(1);
+	}
+
+	~shared_ptr()
+	{
+		if (smart != nullptr)
+		{
+			(*count)--;
+			if ((*count) == 0)
+			{
+				delete smart;
+				delete count;
+				smart = nullptr;
+				count = nullptr;
+			}
+		}
+	}
+
+	shared_ptr(const shared_ptr & other)
+	{
+		if (other.get() != nullptr)
+		{
+			smart = other.get();
+			count = other.c_get();
+			(*count)++;
+		}
+		else
+		{
+			smart = nullptr;
+			count = nullptr;
+		}
+	}
+
+	shared_ptr& operator=(const shared_ptr & other)
+	{
+		if (this != &other)
+		{
+			this->~shared_ptr();
+			if (other.get() != nullptr)
+			{
+				smart = other.get();
+				count = other.c_get();
+				(*count)++;
+			}
+			else {
+				smart = nullptr;
+				count = nullptr;
+			}
+		}
+		return *this;
+	}
+
+	template <typename H>
+	shared_ptr(const shared_ptr<H> & other)
+	{
+		if (other.get() != nullptr)
+		{
+			smart = other.get();
+			count = other.c_get();
+			(*count)++;
+		}
+		else
+		{
+			smart = nullptr;
+			count = nullptr;
+		}
+	}
+
+
+	explicit operator bool() const
+	{
+		return smart == nullptr;
+	}
+
+	T* get() const
+	{
+		return smart;
+	}
+
+	long* c_get() const
+	{
+		return count;
+	}
+
+	long use_count() const
+	{
+		if (smart == nullptr) return 0;
+		else return *count;
+	}
+
+	T& operator*() const
+	{
+		return *smart;
+	}
+
+	T* operator->() const
+	{
+		return smart;
+	}
+
+	void swap(shared_ptr& x) noexcept
+	{
+		if (this != &x)
+		{
+			shared_ptr<T> help(*this);
+			*this = x;
+			x = help;
+		}
+	}
+
+	void reset(T *ptr = 0)
+	{
+		shared_ptr<T>(ptr).swap(*this);
+	}
+
+private:
+	T *smart;
+	long *count;
+};
+
+template <typename H, typename  U>
+bool  operator==(const shared_ptr<H>& lhs, const shared_ptr<U>& rhs)
+{
+	return  lhs.get() == rhs.get();
+}
+
+template <class H, class U>
+bool   operator!=(const shared_ptr<H>& lhs, const shared_ptr<U>& rhs)
+{
+	return lhs.get() != rhs.get();
+}
+
+template <class H, class U>
+bool operator<(const shared_ptr<H>& lhs, const shared_ptr<U>& rhs)
+{
+	return lhs.get()<rhs.get();
+}
+
+template <class H, class U>
+bool  operator>(const shared_ptr<H>& lhs, const shared_ptr<U>& rhs)
+{
+	return rhs < lhs;
+}
+
+template <class H, class U>
+bool  operator<=(const shared_ptr<H>& lhs, const shared_ptr<U>& rhs)
+{
+	return !(rhs < lhs);
+}
+
+template <class H, class U>
+bool  operator>=(const shared_ptr<H>& lhs, const shared_ptr<U>& rhs)
+{
+	return !(lhs < rhs);
+}
+
+template <class H>
+bool  operator==(const shared_ptr<H>& lhs, std::nullptr_t rhs)
+{
+	return !lhs;
+}
+
+template <class H>
+bool  operator==(std::nullptr_t lhs, const shared_ptr<H>& rhs)
+{
+	return !rhs;
+}
+
+template <class H>
+bool operator!=(const shared_ptr<H>& lhs, std::nullptr_t rhs)
+{
+	return (bool)lhs;
+}
+
+template <class H>
+bool operator!=(std::nullptr_t lhs, const shared_ptr<H>& rhs)
+{
+	return (bool)rhs;
+}
+
+template <class H>
+bool operator<(const shared_ptr<H>& lhs, std::nullptr_t rhs)
+{
+	return lhs.get() < nullptr;
+}
+
+template <class H>
+bool operator<(std::nullptr_t lhs, const shared_ptr<H>& rhs)
+{
+	return nullptr < rhs;
+}
+
+template <class H>
+bool operator>(const shared_ptr<H>& lhs, std::nullptr_t rhs)
+{
+	return nullptr < lhs;
+}
+
+template <class H>
+bool operator>(std::nullptr_t lhs, const shared_ptr<H>& rhs)
+{
+	return rhs < nullptr;
+}
+
+template <class H>
+bool operator<=(const shared_ptr<H>& lhs, std::nullptr_t rhs)
+{
+	return !(nullptr < lhs);
+}
+
+template <class H>
+bool operator<=(std::nullptr_t lhs, const shared_ptr<H>& rhs)
+{
+	return !(rhs < nullptr);
+}
+
+template <class H>
+bool operator>=(const shared_ptr<H>& lhs, std::nullptr_t rhs)
+{
+	return !(lhs < nullptr);
+}
+
+template <class H>
+bool operator>=(std::nullptr_t lhs, const shared_ptr<H>& rhs)
+{
+	return !(nullptr < rhs);
+}
