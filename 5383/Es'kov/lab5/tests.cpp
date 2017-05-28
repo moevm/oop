a8f1d094ed5f218dfc2d1da58319f45b65220a2c
Es'kov <eskov.skorpion@yandex.ru>
2017-05-29 01:17:20 +0300

    Больше тестов

--- 5383/Es'kov/lab5/tests.cpp
+++ 5383/Es'kov/lab5/tests.cpp
@@ -17,8 +17,23 @@ stepik::vector< stepik::shared_ptr<shape> > kappa2 =
 	stepik::shared_ptr<shape>(new circle(50, paint_area_info(500, 500, 250, 250, _hdc, rgb_color(255, 0, 0))))
 };
 
+stepik::vector< stepik::shared_ptr<shape> > kappa3 =
+{
+	stepik::shared_ptr<shape>(new ellipse(100, 220, paint_area_info(500, 500, 750, 250, _hdc, rgb_color(0, 255, 0)))),
+	stepik::shared_ptr<shape>(new ellipse(120, 230, paint_area_info(500, 500, 750, 250, _hdc, rgb_color(0, 255, 0)))),
+	stepik::shared_ptr<shape>(new ellipse(130, 240, paint_area_info(500, 500, 750, 250, _hdc, rgb_color(0, 255, 0))))
+};
+
+stepik::vector< stepik::shared_ptr<shape> > kappa2 =
+{
+	stepik::shared_ptr<shape>(new circle(50, paint_area_info(500, 500, 250, 250, _hdc, rgb_color(255, 0, 0)))),
+	stepik::shared_ptr<shape>(new circle(100, paint_area_info(500, 500, 250, 250, _hdc, rgb_color(255, 0, 0)))),
+	stepik::shared_ptr<shape>(new circle(150, paint_area_info(500, 500, 250, 250, _hdc, rgb_color(255, 0, 0))))
+};
+
 TEST(lab5_test, my_func1){
 	EXPECT_EQ(same_search(kappa1, kappa2), 1);
+	EXPECT_EQ(same_search(kappa3, kappa4), INT_MIN);
 }
 
 TEST(lab5_test, my_func2){
