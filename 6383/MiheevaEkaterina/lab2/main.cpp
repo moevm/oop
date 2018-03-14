31e75ca0c290028b714b5e700d1bcacf86a33a42
katierm <miheevakate98@gmail.com>
2018-03-14 17:42:09 +0300

    2 rotator added

--- 6383/MiheevaEkaterina/lab2/main.cpp
+++ 6383/MiheevaEkaterina/lab2/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-#include "Shape.h"
+//#include "Shape.h"
 #include "Ellipse.h"
 #include "Rectangle.h"
 #include "Square.h"
+//#include "Ellipse.h"
+//#include "Rectangle.h"
+//#include "Square.h"
 
 using namespace std;
 
@@ -11,10 +14,11 @@ int main()
     Colour colour(1,2,4);
     Point p(1.0,2.3),
     p1(3,4);
+    //Shape(colour);
     Ellipse el(colour,3,p,2,3);
     cout<<el;
     try {
-        Rectangle rectangle(colour,2,3);
+       Rectangle rectangle(colour,2,3);
         cout<<rectangle;
     }
     catch (char * msg){
