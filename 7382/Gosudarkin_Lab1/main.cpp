5109f6b7fb3d421e26c20df711fd66dbf709a8d1
DarkFlink <zond.999@yandex.ru>
2019-03-11 00:22:59 +0300

    lab1 edited

--- 7382/Gosudarkin_Lab1/main.cpp
+++ 7382/Gosudarkin_Lab1/main.cpp
@@ -1,5 +1,4 @@
-nclude <assert.h>
-#include <algorithm> // std::copy
+#include <assert.h>
 #include <cstddef> // size_t
 #include <iostream>
 
@@ -11,47 +10,57 @@ public:
   Array(const size_t size = 0)
     : m_size(size)
     , m_array(m_size ? new T[m_size]() : nullptr)
-  {}
-  
+  {
+
+      std::cout << "Default constructor\n";
+  }
+
+  void print()
+  {
+      for(size_t it=0; it < m_size; it++)
+          std::cout << m_array[it] << ", ";
+      std::cout << "\n";
+  }
+
   // copy constr
   Array(const Array& src) : Array(src.size())
   {
+      std::cout << "Copy constructor\n";
       if(src.size())
           for(size_t it=0; it < src.size(); it++)
               m_array[it] = src[it];
   }
-  
+
   // move constr
   Array(const Array&& src) : Array(src.size())
   {
+      std::cout << "Move constructor\n";
       if(src.size())
           for(size_t it=0; it < src.size(); it++)
               m_array[it] = src[it];
-      src.~Array();
   }
 
-  Array& operator =(const Array& src)
-  {   
-      if (this == &src)
-          return *this;
-      
-      Array tmp = src;
+  // copy "=" operator
+  Array& operator=(const Array& src)
+  {
+      std::cout << "Operator = \n";
+
       delete m_array;
-      m_size = tmp.size();
+      m_size = src.size();
       m_array = new T[m_size];
       if(m_size)
           for(size_t it=0; it < m_size; it++)
-              m_array[it] = tmp[it];
+              m_array[it] = src[it];
       else
           m_array = nullptr;
       return *this;
   }
-      
+
   size_t size() const
   {
       return m_size;
-  } 
-    
+  }
+
   T& operator [](const size_t index) const
   {
       assert(index < m_size);
@@ -59,8 +68,27 @@ public:
   }
 
   ~Array() { delete[] m_array; }
-  
+
 private:
     size_t m_size;
     T* m_array;
 };
+
+int main()
+{
+    Array<int> a(10);
+    std::cout << "_________________________________\n";
+
+    Array<int> b(a);
+    std::cout << "_________________________________\n";
+
+    Array<int> arr(23);
+    arr = b;
+    arr.print();
+    std::cout << "_________________________________\n";
+
+    Array<int> ex(std::move(a));
+    std::cout << "_________________________________\n";
+
+    return 0;
+}
