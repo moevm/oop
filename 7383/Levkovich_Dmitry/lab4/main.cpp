ab938e965fc292ff6a798d5534eaca28439286d7
DmLvkvch <dima2231298@gmail.com>
2019-03-25 18:50:37 +0300

    7383 Levkovich lab4

---
+++ 7383/Levkovich_Dmitry/lab4/main.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+namespace stepik{
+template <typename T>
+class shared_ptr
+{
+public:
+  explicit shared_ptr(T *ptr = 0)
+  {
+    my_ptr = ptr;
+    i = new long;
+    *i = 1;
+  }
+
+  ~shared_ptr()
+  {
+        if ((*i) > 0)
+            --(*i);
+        if ((*i) == 0){
+            delete my_ptr;
+            delete i;
+        }
+  }
+
+  shared_ptr(const shared_ptr & other)
+  {
+    my_ptr = other.my_ptr;
+    i = other.i;
+    ++(*i);
+  }
+
+  template<typename Derived>
+  shared_ptr(const shared_ptr<Derived> & other){
+      this->my_ptr = other.my_ptr;
+      this->i = other.i;
+      ++(*i);
+  }
+
+  shared_ptr& operator=(const shared_ptr & other)
+  {
+    if (this != &other){
+        this->~shared_ptr();
+        my_ptr = other.my_ptr;
+        i = other.i;
+        ++(*i);
+    }
+    return *this;
+  }
+  template<typename Derived>
+  shared_ptr& operator=(const shared_ptr<Derived> & other)
+  {
+    if ((void*)my_ptr != (void*)&other.my_ptr){
+        this->~shared_ptr();
+        my_ptr = other.my_ptr;
+        i = other.i;
+        ++(*i);
+    }
+    return *this;
+  }
+
+  explicit operator bool() const
+  {
+    return my_ptr != nullptr;
+  }
+
+  template <typename Derived>
+     bool operator==(const shared_ptr<Derived> & other) const
+     {
+       return my_ptr == other.my_ptr;
+     }
+
+  T* get() const
+  {
+    return my_ptr;
+  }
+
+  long use_count() const
+  {
+    return my_ptr == nullptr ? 0 : (*i);
+  }
+
+  T& operator*() const
+  {
+    return *my_ptr;
+  }
+
+  T* operator->() const
+  {
+    return my_ptr;
+  }
+
+  void swap(shared_ptr& x) noexcept
+  {
+    std::swap(my_ptr,x.my_ptr);
+    std::swap(i,x.i);
+  }
+
+  void reset(T *ptr = 0)
+  {
+    this->~shared_ptr();
+    my_ptr = ptr;
+    i = new long(1);
+  }
+
+private:
+    T* my_ptr;
+    long* i;
+  // data members
+};
+} // namespace stepik
+int main()
+{
+    stepik::shared_ptr<int>(t);
+    return 0;
+}
