12ef8ef5900b5f1bf5ea5efcb58c13d2c0030af0
NikitaKoshmanov <31765932+NikitaKoshmanov@users.noreply.github.com>
2019-04-02 12:55:24 +0300

    Update main.cpp

--- 7304/lab1/main.cpp
+++ 7304/lab1/main.cpp
@@ -1,68 +1,90 @@
 #include <assert.h>
+#include <iostream>
 #include <algorithm> // std::copy
 #include <cstddef> // size_t
-
+using namespace std;
 template<typename T>
 class Array
 {
 public:
-    // (default) constructor
-    Array(const size_t size = 0): m_size(size), m_array(m_size ? new T[m_size]() : nullptr){}
-    
-    //Деструктор
-    ~Array(){
-        delete [] m_array;
+// (default) constructor
+Array(const size_t size = 0)
+    : m_size(size)
+    , m_array(m_size ? new T[m_size]() : nullptr)
+{
+    //cout<<"defalt\n";
+}
+
+const size_t size() const
+{
+    return m_size;
+}
+
+T& operator [](const size_t index)
+{
+    assert(index < m_size);
+
+    return m_array[index];
+}
+Array A(Array&& a){};
+//kon kop
+Array (const Array& a)
+    : m_size(a.m_size)
+{
+    //cout<<"kop"<< endl;
+try {
+
+    if (m_size){
+        m_array = new T[m_size];
+        std::copy(a.m_array,a.m_array+m_size,m_array);
     }
-    
-    //Копирование
-    Array(const Array &copy) : m_size(copy.m_size), m_array(m_size ? new T[m_size]():nullptr){
-        std::copy(copy.m_array, copy.m_array + m_size, m_array);
-    }
-    
-    //Перемещение
-    Array(Array&& other){
-        delete [] m_array;
-        m_size = other.m_size;
-        m_array = other.m_array;
-        other.m_size = 0;
-        other.m_array = nullptr;
-    }
-    
-    //Присваивание
-    Array& operator=(const Array &array) {
-        size_t copied_size = m_size;
-        T copied_array[copied_size];
-        std::copy(m_array, m_array + m_size, copied_array);
-        if(this != &array){
-            try {
-                delete [] m_array;
-                m_size = array.size();
-                m_array = new T[m_size]();
-                std::copy(array.m_array, array.m_array + m_size, m_array);
-            }
-            catch(std::exception &e)
-            {
-                m_size = copied_size;
-                std::copy(copied_array, copied_array + copied_size, m_array);
-            }
+        else
+            m_array = nullptr;
+}
+catch (...) {
+    delete[] m_array;
+    throw;
+}
+}
+//op pr
+Array& operator=(Array& a)
+{
+    //cout<<"===";
+    if (this != &a)
+    {
+        T* tmp_array = nullptr;
+    try {
+        if (a.m_size){
+            tmp_array = new T[a.m_size];
+            std::copy(a.m_array,a.m_array+a.m_size,tmp_array);
         }
-        return *this;
+    else
+        tmp_array = nullptr;
     }
-    
-    
-    const size_t size() const
-    {
-        return m_size;
+    catch (...) {
+        delete[] tmp_array;
+        throw;
     }
-    
-    T& operator [](const size_t index)
-    {
-        assert(index < m_size);
-        
-        return m_array[index];
+    delete[] m_array;
+    m_size = a.m_size;
+    m_array = tmp_array;
     }
-    
+    return *this;
+}
+// mv con
+Array (Array&& a)
+{
+    //cout<<"move\n";
+    m_size = a.m_size;
+    m_array = a.m_array;
+    a.m_array = nullptr;
+}
+// dest
+~Array() {
+    //cout<<"destr\n";
+    delete[] m_array;
+}
 private:
-    size_t m_size;
-    T* m_array;
+size_t m_size;
+T* m_array;
 };
