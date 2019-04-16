3831b60f38dae063869e182c1a6e469454453ec0
Bonn1k <bondarchuk.9128@mail.ru>
2019-04-16 13:34:33 +0300

    [7383] Markova Angelina

--- 7383/Markova_Angelina/lab2/lab2.cpp
+++ 7383/Markova_Angelina/lab2/lab2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
 #include <cstdlib>
+#include <cstring>
 
 using namespace std;
 
@@ -24,20 +24,16 @@ struct Color {             //структура цвета
 class Shape {              //класс фигура (любая)
 public:
 
-    Shape():id(count),angle(0), color(), center() {
+    Shape():id(count),angle(0), color() {
         count++;
     }                      //конструктор по умолчанию, count всегда передаём, фигура изначально не повернута, и вызываем конструктор цвета и координат
 
-    Shape(int id, double curr_angle, Color curr_color, Point curr_point):id(count), angle(curr_angle), color(curr_color), center(curr_point) {
+    Shape(int id, double curr_angle, Color curr_color):id(count), angle(curr_angle), color(curr_color) {
         count++;           //конструктор по заданным значениям
     }
 
     double get_angle() {   //взятие угла фигуры (для печати используем)
         return angle;
-    }
-
-    Point get_center() {   //взятие координат центра фигуры
-        return center;
     }
 
     int get_id() {         //взятие индивидуального адреса фигуры
@@ -54,144 +50,124 @@ public:
         color.blue = b;
     }
 
-    ~Shape() {
-        points.clear();    //очищение массива вершин фигуры (вектор задается массивом структур)
-        sides.clear();     //очищение сторон фигур (массив длин сторон)
+    virtual ~Shape() {
         id--;
     }
 
-    void move(const Point new_place) {       //смещение фигруры, относительно центра (перемещаем центр в новую точку)
-        double dx = new_place.x - center.x;  //изменение координат
-        double dy = new_place.y - center.y;
-        center.x += dx;
-        center.y += dy;
-        for(auto& pnt : points) {            //смещение всех вершин
-            pnt.x += dx;
-            pnt.y += dy;
-        }
-    }
-
-    void rotate(int curr_angle)              //поворот фигуры
-    {
-        angle = angle + curr_angle;          //к текущему повороту добавляем заданный угол (для вывода)
-        curr_angle = curr_angle % 360;       //исключаем кол-во оборотов, берём остаток
-        double rad = curr_angle*M_PI/180;    //перевод градусов в радианы
-        double x, y; 
-        for(auto& pnt : points) {            //для каждой точки
-            x = pnt.x;                       //pnt - текущая координата
-            y = pnt.y;
-            pnt.x =(x - center.x) * cos(rad) - (y - center.y) * sin(rad) + center.x; //матрица поворота
-            pnt.y = (x - center.x) * sin(rad) + (y - center.y) * cos(rad) + center.y;
-        }
-
-    }
-
-    void scale(double multiplier) {          //масштабирование
-        double x;
-        double y;
-        for(auto& pnt : points) {
-            x = pnt.x; 
-            y = pnt.y;
-            pnt.x = center.x + (x - center.x) * multiplier;
-            pnt.y = center.y + (y - center.y) * multiplier;
-        }
-    }
-
+    virtual void move(const Point new_place) = 0;
+    virtual void rotate(int curr_angle) = 0;
+    virtual void scale(double multiplier) = 0;
     virtual void print(ostream &out) = 0;    //определение вертуальной функции, поток вывода
-    virtual void sides_of_shape() = 0;       //подсчет сторон
-    virtual double area() = 0;               //площадь фигуры
+    virtual void data_of_shape() = 0;       
     friend ostream& operator << (ostream &out, Shape* shape); //перегрузка оператора
 
 protected:                                   //возможность изменения в наследниках
     double angle;
     int id;
-    Point center;
     Color color;
-    vector <Point> points;
-    vector <double> sides;
 };
 
 ostream& operator << (ostream& out, Shape* shape) {
     out << "Данные о фигуре:"<<endl;
-    out << "Координаты центра: " <<"("<< shape->get_center().x << ";" << shape->get_center().y << ")" << endl;
     out << "Цвет (RGB): " <<"("<< shape->get_color().red << ","<< shape->get_color().green << ","<< shape->get_color().blue << ")" << endl;
     out << "Угол поворота = " << shape->get_angle() << endl;
     out << "Id: " << shape->get_id() << endl;
-    out << "Площадь = " << shape->area() << endl;
     shape->print(out);
     out << "---------------------------------------------------------------------"<<endl;
     return out;
 }
 
 class Square : public Shape {
+private:
+	Point center;
+	Point *arr;
+	double side, area;
 public:
-    Square(const Point A, const Point center, const Color curr_color) : Shape(count, 0, curr_color, Point(center)) { //?
-        id++;
-        points.clear();                      //очищаешь массив
-        points.push_back(A);                 //левый верхний угол
-        double dif = abs(A.x - center.x);
-        points.push_back(Point(A.x + 2 * dif, A.y));             //B
-        points.push_back(Point(A.x + 2 * dif, A.y - 2 * dif));   //C
-        points.push_back(Point(A.x, A.y - 2 * dif));             //D по часовой
+    	Square(const Point A, const Point _center, const Color curr_color) : Shape(count, 0, curr_color) { 
+        center = _center;
+	arr = new Point[4];
+	memset(arr, 0, sizeof(Point)*4);
+	id++;
+	arr[0] = A;
+	double dif = abs(arr[0].x - center.x);
+	arr[1] = {arr[0].x + 2 * dif, arr[0].y};
+	arr[2] = {arr[0].x + 2 * dif, arr[0].y - 2 * dif};
+	arr[3] = {arr[0].x, arr[0].y - 2 * dif};
     }
 
-    void sides_of_shape() override {         //защита от опечатки, т.е мы используем функцию из shape
-        sides.clear();
-        double a = 2*abs(points[0].x - center.x);
-        sides.push_back(a);
+	void move(const Point new_place) override {       
+        double dx = new_place.x - center.x;  
+        double dy = new_place.y - center.y;
+        center.x += dx;
+        center.y += dy;
+        for(int i = 0; i < 4; i++) {            
+            arr[i].x += dx;
+            arr[i].y += dy;
+        }
     }
+	
+	void rotate(int curr_angle) override {
+        angle = angle + curr_angle;          
+        curr_angle = curr_angle % 360;       
+        double rad = curr_angle*M_PI/180;   
+        double x, y; 
+        for(int i = 0; i < 4; i++) {           
+            x = arr[i].x;                      
+            y = arr[i].y;
+            arr[i].x =(x - center.x) * cos(rad) - (y - center.y) * sin(rad) + center.x; //матрица поворота
+            arr[i].y = (x - center.x) * sin(rad) + (y - center.y) * cos(rad) + center.y;
+        }
 
-    double area() override {
-        sides_of_shape();
-        double area = pow(sides[0], 2.0);
-        return area;
+    }
+	
+	void scale(double multiplier) override {         
+        double x, y;
+        for(int i = 0; i < 4; i++) {
+            x = arr[i].x; 
+            y = arr[i].y;
+            arr[i].x = center.x + (x - center.x) * multiplier;
+            arr[i].y = center.y + (y - center.y) * multiplier;
+        }
+	}
+	
+    void data_of_shape() override {         //защита от опечатки, т.е мы используем функцию из shape
+        side = 2 * abs(arr[0].x - center.x);
+	area = side * side;
     }
 
     void print(ostream &out) override {
-        out << "Сторона квадрата: " << sides[0] << endl;
+	data_of_shape();
+	out << "Координаты центра: " <<"("<< center.x << ";" << center.y << ")" << endl;
+	out << "Площадь = " << area << endl;
+        out << "Сторона квадрата: " << side << endl;
         out << "Координаты вершин: ";
-        for(int i = 0; i < points.size(); i++) {
-            out << i << ": (" << points[i].x << "; " << points[i].y << ") || ";
+        for(int i = 0; i < 4; i++) {
+            out << i << ": (" << arr[i].x << "; " << arr[i].y << ") || ";
         }
         out << endl;
     }
 
-    ~Square() {}
+    ~Square() {
+	delete[] arr;
+        id--;
+	}
 };
 
 class Pentagon : public Shape {
-public:
-    Pentagon(const Point center, const Point A, const Color curr_color) : Shape(count, 0, curr_color, Point(center)) {
-        id++;
-        points.clear();
-        push_points(A);
+private:
+	Point center;
+	Point *arr;
+	double side, area;
+	
+	void push_points(Point A) {
+        arr[0] = A;
+	Point tmp;
+	tmp = A;
+	for (int i = 1; i < 5; i++) 
+		arr[i] = rotate_point(tmp, 72);
     }
-
-    void push_points(Point A) {
-        points.push_back(A);
-        int c = 0; //счетчик для цикла
-        Point tmp; //временные координаты
-        tmp.x = A.x;
-        tmp.y = A.y;
-        while (c < 4) {
-            points.push_back(rotate_point(tmp, 72));
-            c++;
-        }
-    }
-
-    void sides_of_shape() override {
-        sides.clear();
-        double n = sqrt(pow((points[0].x - points[1].x), 2.0) + pow((points[0].y - points[1].y), 2.0));
-        sides.push_back(n);
-    }
-
-    double area() override {
-        sides_of_shape();
-        double area = 5*pow(sides[0], 2.0)/(tan(M_PI/5)*4);
-        return abs(area);
-    }
-
-    Point rotate_point(Point &P, int curr_angle) {
+	
+	Point rotate_point(Point &P, int curr_angle) {
         double rad = curr_angle*M_PI/180;
         double x, y;
         x = P.x;
@@ -200,65 +176,161 @@ public:
         P.y = (x - center.x) * sin(rad) - (y - center.y) * cos(rad) + center.y;
         return P;
     }
-
-    void print(ostream &out) override {
-        int i = 0;
-        out << "Сторона правильного пятиугольника: " << sides[0] << endl;
-        out << "Координаты вершин: " ;
-        for(int i = 0; i < points.size(); i++) {
-            out << i << ": (" << points[i].x << "; " << points[i].y << ") || ";
+	
+public:
+    Pentagon(const Point _center, const Point A, const Color curr_color) : Shape(count, 0, curr_color) {
+        center = _center;
+	id++;
+        arr = new Point[5];
+	memset(arr, 0, sizeof(Point)*5);
+        push_points(A);
+    }
+	
+	void move(const Point new_place) override {       
+        double dx = new_place.x - center.x;  
+        double dy = new_place.y - center.y;
+        center.x += dx;
+        center.y += dy;
+        for(int i = 0; i < 5; i++) {            
+            arr[i].x += dx;
+            arr[i].y += dy;
         }
-        out << endl;
+    }
+	
+	void rotate(int curr_angle) override {
+        angle = angle + curr_angle;          
+        curr_angle = curr_angle % 360;       
+        double rad = curr_angle*M_PI/180;   
+        double x, y; 
+        for(int i = 0; i < 5; i++) {           
+            x = arr[i].x;                      
+            y = arr[i].y;
+            arr[i].x =(x - center.x) * cos(rad) - (y - center.y) * sin(rad) + center.x; //матрица поворота
+            arr[i].y = (x - center.x) * sin(rad) + (y - center.y) * cos(rad) + center.y;
+        }
 
     }
+	
+	void scale(double multiplier) override {         
+        double x, y;
+        for(int i = 0; i < 5; i++) {
+            x = arr[i].x; 
+            y = arr[i].y;
+            arr[i].x = center.x + (x - center.x) * multiplier;
+            arr[i].y = center.y + (y - center.y) * multiplier;
+        }
+	}
 
-    ~Pentagon() {}
+    void data_of_shape() override {
+        side = sqrt(pow((arr[0].x - arr[1].x), 2.0) + pow((arr[0].y - arr[1].y), 2.0));
+	area = 5*pow(side, 2.0)/(tan(M_PI/5)*4);
+    }
+
+    void print(ostream &out) override {
+	data_of_shape();
+	out << "Координаты центра: " <<"("<< center.x << ";" << center.y << ")" << endl;
+	out << "Площадь = " << area << endl;
+        out << "Сторона правильного пятиугольника: " << side << endl;
+        out << "Координаты вершин: " ;
+        for(int i = 0; i < 5; i++) {
+            out << i << ": (" << arr[i].x << "; " << arr[i].y << ") || ";
+        }
+        out << endl;
+    }
+
+    ~Pentagon() {
+	delete[] arr;
+        id--;
+	}
 };
 
 class Fivepointed_star: public Pentagon {
+private:
+	Point center;
+	Point *arr;
+	double side, area;
 public:
-    Fivepointed_star(const Point center, double radius_big, const Color curr_color) : Pentagon(Point(center), Point(center.x + radius_big, center.y), curr_color) {
-        id++;
-        points.clear();
+    Fivepointed_star(const Point _center, double radius_big, const Color curr_color) : Pentagon(Point(_center), Point(center.x + radius_big, center.y), curr_color) {
+        center = _center;
+	arr = new Point[10];
+	memset(arr, 0, sizeof(Point)*10);
         Point A = Point(center.x + radius_big, center.y);
-        push_points(A);
-        Point F = Point(center.x + (radius_big*cos(2*M_PI/5)/cos(M_PI/5)), center.y);
-        rotate_point(F, 36);
-        push_points(F);
+	Point tmp = {center.x + (radius_big*cos(2*M_PI/5)/cos(M_PI/5)), center.y};
+	arr[0] = A;
+	arr[1].x = (tmp.x - center.x) * cos(36*M_PI/180) - (tmp.y - center.y) * sin(36*M_PI/180) + center.x;
+	arr[1].y = (tmp.x - center.x) * sin(36*M_PI/180) - (tmp.y - center.y) * cos(36*M_PI/180) + center.y;
+	double rad = 72*M_PI/180;
+	for (int i = 2; i < 10; i++) { 
+		arr[i].x =(arr[i-2].x - center.x) * cos(rad) - (arr[i-2].y - center.y) * sin(rad) + center.x;
+		arr[i].y = (arr[i-2].x - center.x) * sin(rad) - (arr[i-2].y - center.y) * cos(rad) + center.y;
+	}
     }
 
-    void sides_of_shape() override {
-        sides.clear();
-        double n = sqrt(pow((points[0].x - points[5].x), 2.0) + pow((points[0].y - points[5].y), 2.0));
-        sides.push_back(n);
-    }
-
-    double area() override {
-        sides_of_shape();
-        double AB = sides[0];
-        double BC = abs(points[0].x - center.x)*cos(2*M_PI/5)/cos(M_PI/5); //соотношение радиусов вписанной и описанной окружностей
-        double CA = abs(points[0].x - center.x);
+    void data_of_shape() override {
+        side = sqrt(pow((arr[0].x - arr[1].x), 2.0) + pow((arr[0].y - arr[1].y), 2.0));
+	double AB = side;
+        double BC = abs(arr[0].x - center.x)*cos(2*M_PI/5)/cos(M_PI/5); //соотношение радиусов вписанной и описанной окружностей
+        double CA = abs(arr[0].x - center.x);
         double p = (AB+BC+CA)/2;
-        double area = 10 * sqrt(p*(p-AB)*(p-BC)*(p-CA));                   //по формуле Герона
-        return area;
+        area = 10 * sqrt(p*(p-AB)*(p-BC)*(p-CA));
     }
 
     void print(ostream &out) override {
-        int i, j = 0;
-        out << "Сторона пятиконечной звезды: " << sides[0] << endl;
+        data_of_shape();
+	out << "Координаты центра: " <<"("<< center.x << ";" << center.y << ")" << endl;
+	out << "Площадь = " << area << endl;
+        out << "Сторона пятиконечной звезды: " << side << endl;
         out << "Координаты вершин: " ;
-        for(i = 0; i < points.size() - 5; i++) {
-            out << i << ": (" << points[i].x << "; " << points[i].y << ") || ";
+        for(int i = 0; i <  10; i = i + 2) {
+            out << i << ": (" << arr[i].x << "; " << arr[i].y << ") || ";
         }
         out << endl;
         out << "Координаты пересечений: " ;
-        for(j = 5; j < points.size(); j++) {
-            out << j << ": (" << points[j].x << "; " << points[j].y << ") || ";
+        for(int j = 1; j < 10; j = j + 2) {
+            out << j << ": (" << arr[j].x << "; " << arr[j].y << ") || ";
         }
         out << endl;
     }
+	
+	void move(const Point new_place) override {       
+        double dx = new_place.x - center.x;  
+        double dy = new_place.y - center.y;
+        center.x += dx;
+        center.y += dy;
+        for(int i = 0; i < 10; i++) {            
+            arr[i].x += dx;
+            arr[i].y += dy;
+        }
+    }
+	
+	void rotate(int curr_angle) override {
+        angle = angle + curr_angle;          
+        curr_angle = curr_angle % 360;       
+        double rad = curr_angle*M_PI/180;   
+        double x, y; 
+        for(int i = 0; i < 10; i++) {           
+            x = arr[i].x;                      
+            y = arr[i].y;
+            arr[i].x =(x - center.x) * cos(rad) - (y - center.y) * sin(rad) + center.x; //матрица поворота
+            arr[i].y = (x - center.x) * sin(rad) + (y - center.y) * cos(rad) + center.y;
+        }
 
-    ~Fivepointed_star() {}
+    }
+	
+	void scale(double multiplier) override {         
+        double x, y;
+        for(int i = 0; i < 10; i++) {
+            x = arr[i].x; 
+            y = arr[i].y;
+            arr[i].x = center.x + (x - center.x) * multiplier;
+            arr[i].y = center.y + (y - center.y) * multiplier;
+        }
+	}
+
+    ~Fivepointed_star() {
+	delete[] arr;
+        id--;
+	}
 };
 
 int main() {
@@ -270,7 +342,7 @@ int main() {
     cout << test2;
     test2->scale(3);
     cout << test2;
-    Shape* test3 = new Fivepointed_star ({1,4}, 8, {0, 1, 0});
+    Pentagon* test3 = new Fivepointed_star ({1,4}, 8, {0, 1, 0});
     cout << test3;
     test3->move({5, 10});
     cout << test3;
