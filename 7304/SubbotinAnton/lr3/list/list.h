3f8e0e4453890713be970d1618d2cb4d43de3aaa
DesMy <toscha.subbotin2014@yandex.ru>
2019-05-21 09:20:39 +0300

    Correct report && files

--- 7304/SubbotinAnton/lr3/list/list.h
+++ 7304/SubbotinAnton/lr3/list/list.h
@@ -1,3 +1,6 @@
+#ifndef LIST_H
+#define LIST_H
+
 #include <iostream>
 #include <assert.h>
 #include <algorithm>
@@ -315,39 +318,4 @@ namespace stepik
 
 }
 
-
-
-
-
-using namespace stepik;
-using namespace std;
-
-int main()
-{
-
-    list<int> a;
-        cout << "step 1: " << a;
-        a.push_back(3);
-        a.push_back(6);
-        a.push_back(9);
-        a.push_front(2);
-        a.push_front(1);
-        cout << "step 2: " << a;
-
-
-
-        list<int> b(a);
-        b.pop_back();
-        cout << "step 3: " << b;
-        a.clear();
-        a.push_back(0);
-        cout << "step 4: " << a;
-
-        if(a.begin() == b.begin())
-            cout << "step 5: a.begin() == c.begin()" << endl;
-        else
-            cout << "step 5: a.begin() != c.begin()" << endl;
-        a.insert(++a.begin(), 7);
-        cout << "step 6: " << a;
-    return 0;
-}
+#endif // LIST_H
--- 7304/SubbotinAnton/lr3/vector/vector.h
+++ 7304/SubbotinAnton/lr3/vector/vector.h
@@ -1,3 +1,6 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
 #include <iostream>
 #include <assert.h>
 #include <algorithm> // std::copy, std::rotate
@@ -196,30 +199,4 @@ namespace stepik
   };
 }
 
-using namespace stepik;
-using std::cout;
-
-int main(){
-    vector<int> vec1(5);
-        vec1[0] = 0;
-        vec1[1] = 1;
-        vec1[2] = 2;
-        vec1[3] = 3;
-        vec1[4] = 4;
-        cout << "step 1: " << vec1;
-        vec1.erase(vec1.end());
-        cout << "step 2: " << vec1;
-        vec1.push_back(585);
-        cout << "step 3: " << vec1;
-
-        vector<int> vec2(vec1);
-        cout << "step 4: " << vec2;
-        vector<int> vec3(1);
-        vec3 = vec1;
-        cout << "step 5: " << vec3;
-
-        vec1.insert(vec1.begin() + 1, vec2.begin(), vec2.end());
-        cout << "step 6: " << vec1;
-
-    return 0;
-}
+#endif // VECTOR_H
