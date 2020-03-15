80d6d40d2e7a9ec5b7b4c425995af096e5a594e8
kera0106 <42892620+kera0106@users.noreply.github.com>
2020-03-15 19:04:03 +0300

    Zhurbin Kirill lab 3 done
    Zhurbin Kirill lab 3 done

---
+++ 8303/Zhurbin_Kirill/lab 3/source/Mediator.h
@@ -0,0 +1,16 @@
+#ifndef OOP1_MEDIATOR_H
+#define OOP1_MEDIATOR_H
+
+#include "Field.h"
+
+class Mediator{
+public:
+    Mediator(Field* field);
+
+    void attack(Unit* unit, int x, int y);
+
+private:
+    Field* field;
+};
+
+#endif //OOP1_MEDIATOR_H
