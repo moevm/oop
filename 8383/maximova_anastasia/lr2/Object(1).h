ba2bd59541e1673bdcf2cde89b18b4042a7025d7
AlexxxMaxxx <anastasia_maxim@bk.ru>
2020-04-06 15:35:00 +0300

    [8383] Maximova Anastasiia lr2

---
+++ 8383/maximova_anastasia/lr2/Object(1).h
@@ -0,0 +1,18 @@
+#pragma once
+
+class Object { //абстрактный класс
+protected:
+	int x;
+	int y;
+	int view;
+	char name[2];
+
+public:
+	virtual void setName(char symbol) = 0; 
+	virtual void setView(int view) = 0; 
+	virtual void setXY(int x, int y) = 0; 
+	virtual char* getName() = 0; 
+	virtual int getView() = 0;
+	virtual int getX() = 0; 
+	virtual int getY() = 0; 
+};
