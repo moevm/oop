8255b37f9ffeec5a6ac1cabb244eab9e7660ce53
Alexander <alexandr-altukhov2013@yandex.ru>
2020-04-06 23:22:16 +0300

    lab3

---
+++ 8304/Altukhov Alexander/lab3/InputProcessor.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "AttackCommand.h"
+#include "MoveCommand.h"
+#include "CreateCommand.h"
+#include "BaseLogCommand.h"
+
+class Base;
+
+class InputProcessor {
+	std::string input;
+	Base* base;
+
+	std::vector<std::string> split(const std::string& s, char delim);
+	bool checkArguments(std::vector<std::string>& args, int numberOfArgs);
+public:
+	InputProcessor(Base*& base) :base(base) {}
+	bool processString(std::string& input);//true - exit
+	void attack(std::vector<std::string>& args);
+	void move(std::vector<std::string>& args);
+	void create(std::vector<std::string>& args);
+	void baseLog();
+
+};
+
