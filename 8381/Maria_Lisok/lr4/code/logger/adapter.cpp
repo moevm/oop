4a2693c22f3050ad8c40b810567479b225156cd9
marilisok <mari.lisok@mail.ru>
2020-04-20 22:01:31 +0300

    lr4 done

---
+++ 8381/Maria_Lisok/lr4/code/logger/adapter.cpp
@@ -0,0 +1,6 @@
+#include "adapter.h"
+
+Adapter::Adapter()
+{
+
+}
---
+++ 8381/Maria_Lisok/lr4/code/logger/adapter.h
@@ -0,0 +1,31 @@
+#ifndef ADAPTER_H
+#define ADAPTER_H
+#include <map>
+#include "logger.h"
+
+#include "enums.h"
+
+class Adapter
+{
+public:
+    Adapter(Logger*);
+    ~Adapter();
+    void setLogger(Logger *value);
+    void makeLog(Actions act, map<string, int> data);
+private:
+    string userAction(map<string, int> data);
+    string baseAddLog(map<string, int> data);
+    string neutralAddLog(map<string, int> data);
+    string unitAddLog(map<string, int> data);
+    string attackLog(map<string, int> data);
+    string moveLog(map<string, int> data);
+    string gameInfoLog(map<string, int> data);
+    string baseInfoLog(map<string, int> data);
+    string userInfoLog(map<string, int> data);
+    string neutralInfoLog(map<string, int> data);
+    string landCellInfo(map<string, int> data);
+    Logger* logger;
+
+};
+
+#endif // ADAPTER_H
